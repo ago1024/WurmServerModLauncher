041a5e064ecf6429233aa58ccc848e86fbba2317
Alexander Gottwald <alexander.gottwald@gmail.com>
2015-10-26 00:10:55 +0100

    fix dir separator

--- modlauncher.bat
+++ modlauncher.bat
@@ -1,1 +1,1 @@
-runtime/bin/java "-Dworkdir=%CD%" "-Djava.library.path=%CD%\nativelibs" -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar modlauncher.jar
+runtime\bin\java "-Dworkdir=%CD%" "-Djava.library.path=%CD%\nativelibs" -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar modlauncher.jar
