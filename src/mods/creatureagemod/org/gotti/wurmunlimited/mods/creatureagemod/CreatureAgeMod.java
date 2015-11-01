91d60d5c738e1ad2fa2d0f36876ceffc3c7ce30b
Alexander Gottwald <alexander.gottwald@gmail.com>
2015-11-01 15:24:46 +0100

    Exclude certain creatures from increased aging

--- src/mods/creatureagemod/org/gotti/wurmunlimited/mods/creatureagemod/CreatureAgeMod.java
+++ src/mods/creatureagemod/org/gotti/wurmunlimited/mods/creatureagemod/CreatureAgeMod.java
@@ -3,7 +3,12 @@ package org.gotti.wurmunlimited.mods.creatureagemod;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,6 +21,7 @@ import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
 
 import com.wurmonline.server.WurmCalendar;
 import com.wurmonline.server.creatures.CreatureStatus;
+import com.wurmonline.server.creatures.CreatureTemplateIds;
 
 public class CreatureAgeMod implements WurmMod, Configurable {
 
@@ -23,17 +29,64 @@ public class CreatureAgeMod implements WurmMod, Configurable {
 
 	private int increaseGrowthUntilAge = 8;
 	private long increaseGrowthTimer = 259200L;
+	private Set<Integer> excludedTemplates = new HashSet<>(Arrays.asList(
+			CreatureTemplateIds.BOAR_FO_CID,
+			CreatureTemplateIds.HYENA_LIBILA_CID,
+			CreatureTemplateIds.WORG_CID,
+			CreatureTemplateIds.GORILLA_MAGRANON_CID,
+			CreatureTemplateIds.ZOMBIE_CID,
+			CreatureTemplateIds.SKELETON_CID
+			));
 
 	private Logger logger = Logger.getLogger(this.getClass().getName());
 
 	@Override
 	public void configure(Properties properties) {
+		Map<String, Integer> nameToId = new HashMap<>();
+		Map<Integer, String> idToName = new HashMap<>();
+		for (Field field : CreatureTemplateIds.class.getFields()) {
+			String name = field.getName().toLowerCase();
+			if (name.endsWith("_cid")) {
+				name = name.replaceAll("_cid$", "");
+				try {
+					int id = field.getInt(CreatureTemplateIds.class);
+					nameToId.put(name, id);
+					idToName.put(id, name);
+				} catch (IllegalAccessException e) {
+					logger.log(Level.WARNING, null, e);
+				}
+			}
+		}
+		
+		
 		this.increaseGrowthUntilAge = Integer.valueOf(properties.getProperty("increaseGrowthUntilAge", Integer.toString(increaseGrowthUntilAge)));
 		this.increaseGrowthTimer = Math.min(ORIG_CREATURE_POLL_TIMER, Long.valueOf(properties.getProperty("increaseGrowthTimer", Long.toString(increaseGrowthTimer))));
+		
+		String excl = properties.getProperty("excludedTemplates");
+		if (excl != null) {
+			excludedTemplates = new HashSet<>();
+			for (String name : excl.split(",")) {
+				Integer id = nameToId.get(name);
+				if (id == null) {
+					try {
+						id = Integer.parseInt(name);
+					} catch (NumberFormatException e) {
+						id = null;
+					}
+				}
+				if (id == null) {
+					logger.warning("Invalid template " + id);
+					continue;
+				}
+				excludedTemplates.add(id);
+			}
+		}
 
 		logger.log(Level.INFO, "increaseGrowthUntilAge: " + increaseGrowthUntilAge);
 		logger.log(Level.INFO, "increaseGrowthTimer: " + increaseGrowthTimer);
-
+		
+		Iterable<String> iterable = () -> excludedTemplates.stream().map((id) -> idToName.get(id)).iterator();
+		logger.log(Level.INFO, "excludedTemplates: " + String.join(",", iterable));
 	}
 
 	@Override
@@ -56,13 +109,14 @@ public class CreatureAgeMod implements WurmMod, Configurable {
 							Boolean value = ReflectionUtil.getPrivateField(creatureStatus, field);
 							boolean reborn = value != null && value.booleanValue();
 							int age = creatureStatus.age;
+							int templateId = creatureStatus.getTemplate().getTemplateId();
 
 							// Check if the shorter timer applies and has elapsed
-							if (!reborn && age < increaseGrowthUntilAge && WurmCalendar.currentTime - creatureStatus.lastPolledAge > increaseGrowthTimer) {
+							if (!reborn && age < increaseGrowthUntilAge && WurmCalendar.currentTime - creatureStatus.lastPolledAge > increaseGrowthTimer && !excludedTemplates.contains(templateId)) {
 								long origLastPolled = creatureStatus.lastPolledAge;
 
 								// Set a fake last polled time earlier in time
-								long newLastPolled = WurmCalendar.currentTime - ORIG_CREATURE_POLL_TIMER;
+								long newLastPolled = WurmCalendar.currentTime - ORIG_CREATURE_POLL_TIMER - 1;
 								creatureStatus.lastPolledAge = newLastPolled;
 
 								// run pollAge()
