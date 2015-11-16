0db217d4287c4b3a02003a9234909dab8e3c8159
Alexander Gottwald <alexander.gottwald@gmail.com>
2015-11-16 18:45:26 +0100

    Set LD_LIBRARY_PATH in .sh script

--- modlauncher.sh
+++ modlauncher.sh
@@ -2,4 +2,4 @@
 if test -e "logging.properties"; then
 	LOGGING=-Djava.util.logging.config.file=logging.properties
 fi
-./runtime/jre1.8.0_60/bin/java "-Dworkdir=$PWD" "-Djava.library.path=$PWD/nativelibs" $LOGGING -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar ./modlauncher.jar "$*"
+LD_LIBRARY_PATH="$PWD/nativelibs" ./runtime/jre1.8.0_60/bin/java "-Dworkdir=$PWD" "-Djava.library.path=$PWD/nativelibs" $LOGGING -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar ./modlauncher.jar "$*"
