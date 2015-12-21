105108138382eab2f55304bc340ce9e2e77ac9d4
Alexander Gottwald <alexander.gottwald@gmail.com>
2015-12-21 10:40:18 +0100

    Enhanced detection of shard lib folder and java path on linux

--- modlauncher.sh
+++ modlauncher.sh
@@ -2,4 +2,22 @@
 if test -e "logging.properties"; then
 	LOGGING=-Djava.util.logging.config.file=logging.properties
 fi
-LD_LIBRARY_PATH="$PWD/nativelibs" ./runtime/jre1.8.0_60/bin/java "-Dworkdir=$PWD" "-Djava.library.path=$PWD/nativelibs" $LOGGING -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar ./modlauncher.jar "$*"
+
+
+if test -x "runtime/jre1.8.0_60/bin/java"; then
+	JAVA="runtime/jre1.8.0_60/bin/java"
+elif test -x "../runtime/jre1.8.0_60/bin/java"; then
+	JAVA="../runtime/jre1.8.0_60/bin/java"
+else
+	JAVA="java"
+fi
+
+LD_LIBRARY_PATH="$PWD/nativelibs"
+if test $(uname -m) = "x86_64"; then
+	LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/linux64"
+else
+	LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD"
+fi
+export LD_LIBRARY_PATH
+
+"$JAVA" "-Dworkdir=$PWD" "-Djava.library.path=$PWD/nativelibs" $LOGGING -Xmn256M -Xms512m -Xmx2048m -XX:+OptimizeStringConcat -XX:+AggressiveOpts -jar ./modlauncher.jar "$*"
