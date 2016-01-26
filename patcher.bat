b938b58646d9df3a0d2d258f84a39340f0d2a14d
Alexander Gottwald <alexander.gottwald@gmail.com>
2016-01-26 08:41:47 +0100

    Added pause to patcher script

--- patcher.bat
+++ patcher.bat
@@ -3,3 +3,4 @@ if exist runtime\bin\java.exe set JAVA=runtime\bin\java.exe
 if exist ..\runtime\bin\java.exe set JAVA=..\runtime\bin\java.exe
 
 %JAVA% -classpath patcher.jar;javassist.jar org.gotti.wurmunlimited.patcher.PatchServerJar
+pause
