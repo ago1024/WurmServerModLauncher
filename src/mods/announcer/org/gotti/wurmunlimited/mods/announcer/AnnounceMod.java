fb4a84ce88a124d48eab20bbccd8b69b965156e4
Alexander Gottwald <alexander.gottwald@gmail.com>
2016-02-09 18:41:36 +0100

    Added announcer mod
    Server-wide player login announcement

---
+++ src/mods/announcer/org/gotti/wurmunlimited/mods/announcer/AnnounceMod.java
@@ -0,0 +1,42 @@
+package org.gotti.wurmunlimited.mods.announcer;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
+import org.gotti.wurmunlimited.modloader.interfaces.Initable;
+import org.gotti.wurmunlimited.modloader.interfaces.PlayerLoginListener;
+import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
+import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
+
+import com.wurmonline.server.MessageServer;
+import com.wurmonline.server.players.Player;
+
+public class AnnounceMod implements WurmMod, Initable, PreInitable, Configurable, PlayerLoginListener {
+
+	boolean announcePlayers = true;
+
+	@Override
+	public void configure(Properties properties) {
+		announcePlayers = Boolean.valueOf(properties.getProperty("announcePlayrs", String.valueOf(announcePlayers)));
+
+		Logger.getLogger(AnnounceMod.class.getName()).log(Level.INFO, "announcePlayers: " + announcePlayers);
+	}
+
+	@Override
+	public void preInit() {
+	}
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void onPlayerLogin(Player player) {
+		if (announcePlayers) {
+			MessageServer.broadCastSafe("Player " + player.getName() + " has logged in.", (byte) 1);
+		}
+	}
+
+}
