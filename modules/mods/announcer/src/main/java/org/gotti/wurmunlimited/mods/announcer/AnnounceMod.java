80ccb3767a34cb66aa8151225b7af12587a34a5b
Alexander Gottwald <alexander.gottwald@gmail.com>
2016-06-10 18:41:25 +0200

    Do not announce GMs by default

--- modules/mods/announcer/src/main/java/org/gotti/wurmunlimited/mods/announcer/AnnounceMod.java
+++ modules/mods/announcer/src/main/java/org/gotti/wurmunlimited/mods/announcer/AnnounceMod.java
@@ -16,12 +16,15 @@ import com.wurmonline.server.players.Player;
 public class AnnounceMod implements WurmServerMod, Initable, PreInitable, Configurable, PlayerLoginListener {
 
 	boolean announcePlayers = true;
+	int announceMaxPower = 0;
 
 	@Override
 	public void configure(Properties properties) {
-		announcePlayers = Boolean.valueOf(properties.getProperty("announcePlayrs", String.valueOf(announcePlayers)));
+		announcePlayers = Boolean.valueOf(properties.getProperty("announcePlayers", String.valueOf(announcePlayers)));
+		announceMaxPower = Integer.valueOf(properties.getProperty("announceMaxPower", String.valueOf(announceMaxPower)));
 
 		Logger.getLogger(AnnounceMod.class.getName()).log(Level.INFO, "announcePlayers: " + announcePlayers);
+		Logger.getLogger(AnnounceMod.class.getName()).log(Level.INFO, "announceMaxPower: " + announceMaxPower);
 	}
 
 	@Override
@@ -34,7 +37,7 @@ public class AnnounceMod implements WurmServerMod, Initable, PreInitable, Config
 
 	@Override
 	public void onPlayerLogin(Player player) {
-		if (announcePlayers) {
+		if (announcePlayers && player.getPower() <= announceMaxPower) {
 			MessageServer.broadCastSafe("Player " + player.getName() + " has logged in.", (byte) 1);
 		}
 	}
