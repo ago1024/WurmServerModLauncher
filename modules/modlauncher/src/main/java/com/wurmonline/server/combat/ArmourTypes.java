162d7df27accca17ebc8ee421b9639a7648502af
Alexander Gottwald <alexander.gottwald@gmail.com>
2018-09-06 07:50:22 +0200

    Compatibility armour types

---
+++ modules/modlauncher/src/main/java/com/wurmonline/server/combat/ArmourTypes.java
@@ -0,0 +1,16 @@
+package com.wurmonline.server.combat;
+
+@Deprecated
+public interface ArmourTypes {
+	public static final int ARMOUR_LEATHER = 1;
+	public static final int ARMOUR_STUDDED = 2;
+	public static final int ARMOUR_CHAIN = 3;
+	public static final int ARMOUR_PLATE = 4;
+	public static final int ARMOUR_RING = 5;
+	public static final int ARMOUR_CLOTH = 6;
+	public static final int ARMOUR_SCALE = 7;
+	public static final int ARMOUR_SPLINT = 8;
+	public static final int ARMOUR_LEATHER_DRAGON = 9;
+	public static final int ARMOUR_SCALE_DRAGON = 10;
+	public static final int ARMOUR_NONE = -1;
+}
